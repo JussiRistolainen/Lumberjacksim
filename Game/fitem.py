919307e4c4484033d7c2817d471065772b1ac0f7
Dan Nasman <dan.nasman@gmail.com>
2021-04-17 09:20:51 +0300

    removed unnecessary function calls from fitem.py

--- Game/fitem.py
+++ Game/fitem.py
@@ -6,12 +6,3 @@ class Item(Position):
         super(Item, self).__init__(x, y)
         self.window = window
         self.name = name
-
-    def getPosition(self):
-        return self.x, self.y
-
-    def getPositionX(self):
-        return self.x
-
-    def getPositionY(self):
-        return self.y
--- Game/item_test.py
+++ Game/item_test.py
@@ -6,7 +6,7 @@ def main():
     item = Item(pygame.display.set_mode((0, 0)), 0, 0, "item")
     item2 = Item(pygame.display.set_mode((0, 0)), 4, 0, "item")
     print(item.getDistanceFromPosition(item2))
-    print(item.getPositionX)
+    print(item.getPositionX())
 
 
 main()
